be4b44056e109f8b5c71607c87818358fba855f7
ganaixia <wanghuigan@126.com>
2022-08-17 17:28:58 +0800

    pagination静态全局组件

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 
 import TypeNav from '@/components/TypeNav'
 import Carousel from '@/components/Carousel'
+import Pagination from "@/components/Pagination";
 import router from '@/router'
 import store from './store'
 import '@/mock/mockServe'
 import 'swiper/css/swiper.css'
 Vue.component(TypeNav.name, TypeNav)
 Vue.component(Carousel.name, Carousel)
+Vue.component(Pagination.name, Pagination)
 
 new Vue({
   router,
