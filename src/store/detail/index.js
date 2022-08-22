599e29fb3277f28ca6fbc9a3e4348d81f83239ba
ganaixia <wanghuigan@126.com>
2022-08-22 16:04:45 +0800

    详情页面静态数据

--- src/store/detail/index.js
+++ src/store/detail/index.js
@@ -17,7 +17,21 @@ let actions = {
         }
     }
 }
-let getters = {}
+//简化数据
+let getters = {
+    //路径导航
+    categoryView(state){
+        return state.goodInfo.categoryView || {}
+    },
+    //产品信息
+    skuInfo(state){
+        return state.goodInfo.skuInfo || {}
+    },
+    //产品售卖属性
+    spuSaleAttrList(state){
+        return state.goodInfo.spuSaleAttrList || []
+    }
+}
 
 export default {
     state,
